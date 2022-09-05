d45696af147d6d7a8133a875bb05867d4ee6d1d5
Dheeresha <dheeresha.s@sigmainfo.net>
2022-09-05 14:49:14 +0530

    Updated RO list API request params format

--- lib/carserv/public_api/client/repair_order.rb
+++ lib/carserv/public_api/client/repair_order.rb
@@ -7,9 +7,12 @@ module Carserv
       class RepairOrder < Carserv::PublicApi::Client::Base
         class << self
           def list(start_date:, end_date:, page: 1)
+            params = {}
+            params[:filter] = {}
+            params[:filter][:closed_at] = { from: start_date, to: end_date }
             request do
               includes(:appointment, :customer, :vehicle, :repair_shop)
-                .with_params(from: start_date, to: end_date).page(page).all
+                .with_params(params).page(page).all
             end
           end
 
